68d1c193fe46f45fea34d8ef61cc337da9d596b7
charlie_sz <karol.szczepaniec@gopos.pl>
2021-02-18 21:30:08 +0100

    add fies test request GET, POST pathParams queryParams

--- src/main/java/pl/charliesz/ping/boundary/PingResource.java
+++ src/main/java/pl/charliesz/ping/boundary/PingResource.java
@@ -1,24 +1,21 @@
 package pl.charliesz.ping.boundary;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import javax.ws.rs.*;
 
-/**
- *
- * @author airhacks.com
- */
+
 @Path("ping")
 public class PingResource {
 
-    @Inject
-    @ConfigProperty(name = "message")
-    String message;    
-
     @GET
     public String ping() {
-        return this.message + " Jakarta EE 8 with MicroProfile 3+!";
+        return " Jakarta EE 8 with MicroProfile 3+!";
+    }
+
+    @GET
+    @Path("pong/{id}")
+    public String pong(@PathParam("id") Long id, @QueryParam("count") Long c, @QueryParam("func") Long f){
+        return "przesłane id: " + id + " count: " + c + " func: " + f;
     }
 
 }
