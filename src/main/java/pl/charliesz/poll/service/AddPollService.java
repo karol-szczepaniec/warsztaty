a9429b9ef3190b5b1edde0238e5ef60d27362d01
charlie_sz <karol.szczepaniec@gopos.pl>
2021-02-28 11:18:45 +0100

    service

---
+++ src/main/java/pl/charliesz/poll/service/AddPollService.java
@@ -0,0 +1,23 @@
+package pl.charliesz.poll.service;
+
+import pl.charliesz.poll.boundary.PollResource;
+import pl.charliesz.poll.model.PollDto;
+
+import javax.ejb.Stateless;
+import java.util.logging.Logger;
+
+@Stateless
+public class AddPollService {
+
+    private static final Logger LOGGER = Logger.getLogger(AddPollService.class.getName());
+
+    public void addPoll(PollDto poll){
+        // Sprawdzam czy ostatni znak to pytajnik
+        LOGGER.info("Before ensuring '?' : "+ poll.getQuestion());
+        if (poll.getQuestion().charAt(poll.getQuestion().length()-1) != '?') {
+            // jeśli nie to dopisuję pytajnik
+            poll.setQuestion(poll.getQuestion() + '?');
+        }
+        LOGGER.info("After ensuring '?' : " + poll.getQuestion());
+    }
+}
